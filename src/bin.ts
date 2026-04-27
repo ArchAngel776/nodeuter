c090a8d225f0ec9e7d9bd475167a647dc8b9c75b
ArchAngel776 <karol.marciniak155@gmail.com>
2026-04-27 21:30:04 +0200

    refactor: simplify src and move declaration output to @types

--- src/bin.ts
+++
@@ -1,5 +0,0 @@
-import { createKernel } from "@/index";
-
-const kernel = createKernel();
-
-console.log(kernel.start());
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-export { component, getComponentName } from "./decorators/component";
-export type { ComponentConstructor } from "./decorators/component";
-export { createKernel, Kernel } from "./kernel";
+export {};
